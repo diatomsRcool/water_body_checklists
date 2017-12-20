ae9ea8b92c240c11e83381dafb1d831b5cdbe605
jhpoelen <jhpoelen@xs4all.nl>
2017-12-20 10:53:07 -0800

    refactor to re-use existing script to avoid .csv bug; fyi @diatomsRcool

--- checklist_script_gen.sh
+++ checklist_script_gen.sh
@@ -2,18 +2,6 @@
 # generates checklist_status.sh using wkt_string.tsv
 # the generated checklist_status.sh produces checklist_status.tsv
 # checklist_status.tsv contains a status overview of all checklists associated with wkt_string.tsv
+# see https://github.com/effechecka/effechecka_scripts
 
-generate_status_script() {
-  echo "echo \"mrgid\\\tchecked_at\\\thttp_status_code\" > checklist_status.tsv" > checklist_status.sh
-  cat wkt_string.tsv | awk -F '\t'  '{ print "curl -s -o /dev/null -w \"%{http_code}\" \"http://api.effechecka.org/checklist.csv?limit=1&wktString=" $2 "\"| xargs echo " $1 "\\\t$(date -Is)\\\t >> checklist_status.tsv"; }' >> checklist_status.sh
-}
-
-generate_download_script() {
-  script_name="checklist_download.sh"
-  echo "mkdir -p checklist" > $script_name
-  cat wkt_string.tsv | awk -F '\t'  '{ print "curl -o checklist/" $1 ".tsv \"http://api.effechecka.org/checklist.tsv?wktString=" $2 "\""; }' >> $script_name
-  echo "tar czf checklist.tar.gz wkt_string.tsv checklist_* checklist/*" >> $script_name
-}
-
-generate_status_script
-generate_download_script
+bash <(curl -s https://raw.githubusercontent.com/effechecka/effechecka-scripts/master/checklist_script_gen.sh)
